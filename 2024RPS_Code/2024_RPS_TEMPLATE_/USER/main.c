3ccce7976b3dc27f282ecb389eb2f75f584e961f
LeeZekai233 <115407059+LeeZekai233@users.noreply.github.com>
2023-10-11 18:54:24 +0800

    add_new_cooperation_environment

---
+++ 2024RPS_Code/2024_RPS_TEMPLATE_/USER/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+int main()
+{
+	gimbal_parameter_Init();
+   BSP_Init();
+
+	 
+	
+	while(1)
+	{
+		
+
+	}
+}
---
+++ 2024RPS_Code/INFANTRY_DEMO/APP/TASK/MODE_SWITCH_TASK/Inc/infantry_mode_switch_task.h
@@ -0,0 +1,30 @@
+#ifndef __INFANTRY_MODE_SWITCH_TASK_H
+#define __INFANTRY_MODE_SWITCH_TASK_H
+#include "public.h"
+
+
+
+/*************************************************/
+#define HIGH_SPEED 1600
+#define NORMAL_SPEED 550 
+
+
+
+
+
+void infantry_mode_switch_task(void);
+
+
+extern chassis_t chassis;
+
+
+
+
+
+
+
+
+
+
+
+#endif
